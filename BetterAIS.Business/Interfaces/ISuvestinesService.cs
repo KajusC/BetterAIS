aef0ca9d66507473fc8e2912346156628668363b
Vezhys <133751736+vezhys@users.noreply.github.com>
2024-12-12 20:12:01 +0200

    suvestines back not tested

---
+++ BetterAIS.Business/Interfaces/ISuvestinesService.cs
@@ -0,0 +1,22 @@
+﻿using BetterAIS.Business.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterAIS.Business.Interfaces
+{
+    public interface ISuvestinesService
+    {
+        Task<IEnumerable<SuvestinesDTO>> GetAllAsync();
+
+        Task<SuvestinesDTO> GetByIdAsync(int id);
+
+        Task AddAsync(SuvestinesDTO suvestinesModel);
+
+        Task UpdateAsync(SuvestinesDTO suvestinesModel);
+
+        Task DeleteAsync(int id);
+    }
+}
